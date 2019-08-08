6ec67fec77656a1b1f6fe3a63c782c96bc0a4bd5
KangChangMin <kcmschool73@gmail.com>
2019-08-09 06:48:09 +0900

    first-webpack

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const fileOptions = {
+	'css': ['app.bundle.css'],
+	'js': ['app.bundle.js'],
+	'chunks': {
+		'head': {
+			'entry': '',
+			'css': 'app.bundle.css'
+		},
+		'body': {
+			'entry': 'app.bundle.js',
+			'css': ''
+		}
+	}
+}
+
+
+module.exports = {
+	entry: './src/assets/js/app.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'assets/js/app.bundle.js'
+	},
+	mode: 'development',
+	devServer: {
+		contentBase: __dirname + '/dist',
+		port: 3000
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			// {
+			// 	test: /\.s?css$/,
+			// 	use: ['style-loader', 'css-loader', 'sass-loader']
+			// },
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'postcss-loader']
+				})
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader?name=assets/images/',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: '65-90',
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							webp: {
+								quality: 75
+							}
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: './src/assets/images', to: './assets/images' }
+		]),
+
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html',
+			'files': fileOptions
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'intro.html',
+			template: 'src/intro.html',
+			'files': fileOptions
+		}),
+
+		new ExtractTextPlugin('assets/css/app.bundle.css')
+	]
+};
